ac0561cd447aff23e6561e4998270d717af4203e
Roman Tikhonov <spooffit@mail.ru>
2023-07-09 22:26:00 +0300

    Added Character Dtos

---
+++ src/star_wars.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using star_wars.Application.Common.Models;
+using star_wars.Core.Entities;
+
+namespace star_wars.Application.Common.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Character, GetCharacterDto>();
+        CreateMap<AddCharacterDto, Character>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+        CreateMap<UpdateCharacterDto, Character>();
+        CreateMap<UpdateCharacterDto, GetCharacterDto>();
+    }
+}
---
+++ src/star_wars.Application/Common/Models/GetCharacterDto.cs
@@ -0,0 +1,19 @@
+﻿using star_wars.Core.Entities;
+
+namespace star_wars.Application.Common.Models;
+
+public class GetCharacterDto
+{
+    public int Id { get; }
+    public string Name { get; }
+    public string OriginName { get; }
+    public int Birthdate { get; }
+    public Planet Planet { get; }
+    public string Gender { get; }
+    public string Species { get; }
+    public float Height { get; }
+    public string HairColor { get; }
+    public string EyeColor { get; }
+    public string Description { get; }
+    public ICollection<Movie> Movies { get; }
+}
