99f731bd6f4d2c4449d50a37533a2e9ad61f6f3f
Roman Tikhonov <spooffit@mail.ru>
2023-07-10 17:21:56 +0300

    Added CatalogController.cs

---
+++ src/star_wars.Web/Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using star_wars.Application.Common.Interfaces.Services;
+
+
+namespace star_wars.Web.Controllers;
+
+public class CatalogController : Controller
+{
+    private readonly ICharacterService _characterService;
+    private readonly ICharacterViewModelService _characterViewModelService;
+
+    public CatalogController(
+        ICharacterService service, 
+        ICharacterViewModelService characterViewModelService)
+    {
+        _characterService = service;
+        _characterViewModelService = characterViewModelService;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        var characters = await _characterService.GetAllCharactersAsync();
+        var characterViewModels = _characterViewModelService.MapCharacterListToViewModel(characters);
+
+        return View(characterViewModels);
+    }
+}
