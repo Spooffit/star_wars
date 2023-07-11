78e3832748f0a51f439799ad880663e008d6bfc4
Roman Tikhonov <spooffit@mail.ru>
2023-07-11 11:56:00 +0300

    Changed MappingProfile.cs

--- src/star_wars.Application/Common/Mappings/MappingProfile.cs
+++ src/star_wars.Application/Common/Mappings/MappingProfile.cs
@@ -10,30 +10,13 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Character, GetCharacterDto>();
-        CreateMap<AddCharacterDto, Character>()
-            .ForMember(dest => dest.Id, opt => opt.Ignore());
+        CreateMap<AddCharacterDto, Character>();
         CreateMap<UpdateCharacterDto, Character>();
         CreateMap<UpdateCharacterDto, GetCharacterDto>();
 
+        CreateMap<GetCharacterDto, CharacterViewModel>();
         CreateMap<ICollection<GetCharacterDto>, CharacterListViewModel>()
             .ForMember(dest => dest.Characters, opt =>
-                opt.MapFrom(src =>
-                    src.Select(dto => new CharacterViewModel
-                    {
-                        Id = dto.Id,
-                        Name = dto.Name,
-                        OriginName = dto.OriginName,
-                        Birthdate = dto.Birthdate,
-                        Planet = dto.Planet,
-                        Gender = dto.Gender,
-                        Species = dto.Species,
-                        Height = dto.Height,
-                        HairColor = dto.HairColor,
-                        EyeColor = dto.EyeColor,
-                        Description = dto.Description,
-                        Movies = dto.Movies.ToList()
-                    }).ToList()));
-
-        CreateMap<GetCharacterDto, CharacterViewModel>();
+                opt.MapFrom(src => src.ToList()));
     }
 }
