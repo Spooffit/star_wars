bbf3ea2360067143708699a39bb25275b57d4386
Roman Tikhonov <spooffit@mail.ru>
2023-07-11 16:59:59 +0300

    Added View For CatalogController.cs

--- src/star_wars.Web/Controllers/CatalogController.cs
+++ src/star_wars.Web/Controllers/CatalogController.cs
@@ -17,11 +17,33 @@ public class CatalogController : Controller
         _characterViewModelService = characterViewModelService;
     }
     
+    [HttpGet]
     public async Task<IActionResult> Index()
     {
         var characters = await _characterService.GetAllCharactersAsync();
         var characterViewModels = _characterViewModelService.MapCharacterListToViewModel(characters);
 
         return View(characterViewModels);
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult> Info(int id)
+    {
+        var entity = await _characterService.GetCharacterByIdAsync(id);
+        var characterViewModels = _characterViewModelService.MapCharacterToViewModel(entity);
+        
+        return View(characterViewModels);
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> Edit()
+    {
+        return View();
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> Delete()
+    {
+        return View();
     }
 }
