b20ee93d9d752f730fd3f58c873c185f6656c614
Roman Tikhonov <spooffit@mail.ru>
2023-07-11 11:55:41 +0300

    Changed Character Dtos

--- src/star_wars.Application/Common/Models/Dto/Character/AddCharacterDto.cs
+++ src/star_wars.Application/Common/Models/Dto/Character/AddCharacterDto.cs
@@ -4,15 +4,15 @@ namespace star_wars.Application.Common.Models.Dto.Character;
 
 public class AddCharacterDto
 {
-    public string Name { get; }
-    public string OriginName { get; }
-    public int Birthdate { get; }
-    public Planet Planet { get; }
-    public string Gender { get; }
-    public string Species { get; }
-    public float Height { get; }
-    public string HairColor { get; }
-    public string EyeColor { get; }
-    public string Description { get; }
-    public ICollection<Movie> Movies { get; }
+    public string Name { get; set; }
+    public string OriginName { get; set;  }
+    public int Birthdate { get; set;  }
+    public Planet Planet { get; set;  }
+    public string Gender { get; set;  }
+    public string Species { get; set;  }
+    public float Height { get; set;  }
+    public string HairColor { get; set;  }
+    public string EyeColor { get; set;  }
+    public string Description { get; set;  }
+    public ICollection<Movie> Movies { get; set;  }
 }
