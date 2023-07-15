d3c5e7b8cc1aead980452ca08d5845cd70181e6a
Roman Tikhonov <spooffit@mail.ru>
2023-07-15 07:23:55 +0300

    Changed core entities

--- src/star_wars.Core/Entities/Character.cs
+++ src/star_wars.Core/Entities/Character.cs
@@ -6,12 +6,12 @@ public class Character
     public string Name { get; set; }
     public string OriginName { get; set; }
     public int Birthdate { get; set; }
-    public Planet Planet { get; set; }
+    public string Planet { get; set; }
     public string Gender { get; set; }
     public string Species { get; set; }
     public float Height { get; set; }
     public string HairColor { get; set; }
     public string EyeColor { get; set; }
     public string Description { get; set; }
-    public ICollection<Movie> Movies { get; set; }
+    public List<Movie> Movies { get; set; } = new();
 }
