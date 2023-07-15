93491bea5955dba602b23c3cf5c77922693d7f54
Roman Tikhonov <spooffit@mail.ru>
2023-07-15 07:26:20 +0300

    Added viewModels

---
+++ src/star_wars.Application/Common/Models/ViewModels/Character/EditCharacterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using star_wars.Application.Common.Models.ViewModels.Movie;
+
+namespace star_wars.Application.Common.Models.ViewModels.Character;
+
+public class EditCharacterViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string OriginName { get; set; }
+    public int Birthdate { get; set; }
+    public string Planet { get; set; }
+    public string Gender { get; set; }
+    public string Species { get; set; }
+    public float Height { get; set; }
+    public string HairColor { get; set; }
+    public string EyeColor { get; set; }
+    public string Description { get; set; }
+    public List<MovieViewModel> RestAllMovies { get; set; }
+    public string OwnsMovies { get; set; }
+}
---
+++ src/star_wars.Application/Common/Models/ViewModels/Character/InfoCharacterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using star_wars.Application.Common.Models.ViewModels.Movie;
+
+namespace star_wars.Application.Common.Models.ViewModels.Character;
+
+public class InfoCharacterViewModel
+{
+    public string Name { get; set; }
+    public string OriginName { get; set; }
+    public int Birthdate { get; set; }
+    public string Planet { get; set; }
+    public string Gender { get; set; }
+    public string Species { get; set; }
+    public float Height { get; set; }
+    public string HairColor { get; set; }
+    public string EyeColor { get; set; }
+    public string Description { get; set; }
+    public List<MovieViewModel> Movies { get; set; }
+}
