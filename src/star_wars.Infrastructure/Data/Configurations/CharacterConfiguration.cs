7e46ae27bb23c54a33bf169613b6e0cfcea2cac6
Roman Tikhonov <spooffit@mail.ru>
2023-07-17 05:59:14 +0300

    Changed Configurations

--- src/star_wars.Infrastructure/Data/Configurations/CharacterConfiguration.cs
+++ src/star_wars.Infrastructure/Data/Configurations/CharacterConfiguration.cs
@@ -8,40 +8,40 @@ public class CharacterConfiguration : IEntityTypeConfiguration<Character>
 {
     public void Configure(EntityTypeBuilder<Character> builder)
     {
-        // builder.HasKey(c => c.Id);
-        // builder.HasIndex(c => c.Id)
-        //     .IsUnique();
-        // builder.HasIndex(c => c.Gender);
-        // builder.HasIndex(c => c.Birthdate);
-        // builder.HasIndex(c => c.Species);
-        // builder.Property(c => c.Id)
-        //     .ValueGeneratedOnAdd();
-        //
-        // builder.Property(c => c.Name)
-        //     .HasMaxLength(100)
-        //     .IsRequired();
-        // builder.Property(c => c.OriginName)
-        //     .HasMaxLength(100)
-        //     .IsRequired();
-        // builder.Property(c => c.Birthdate)
-        //     .IsRequired();
-        // builder.Property(c => c.Gender)
-        //     .HasMaxLength(30)
-        //     .IsRequired();
-        // builder.Property(c => c.Species)
-        //     .IsRequired();
-        // builder.Property(c => c.Height)
-        //     .HasPrecision(10,2)
-        //     .IsRequired();
-        // builder.Property(c => c.HairColor)
-        //     .HasMaxLength(30)
-        //     .IsRequired();
-        // builder.Property(c => c.EyeColor)
-        //     .HasMaxLength(30)
-        //     .IsRequired();
-        // builder.Property(c => c.Description)
-        //     .HasMaxLength(2000)
-        //     .IsRequired();
+        builder.HasKey(c => c.Id);
+        builder.HasIndex(c => c.Id)
+            .IsUnique();
+        builder.Property(c => c.Id)
+            .ValueGeneratedOnAdd();
+        builder.HasIndex(c => c.Gender);
+        builder.HasIndex(c => c.Birthdate);
+        builder.HasIndex(c => c.Species);
+
+        builder.Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+        builder.Property(c => c.OriginName)
+            .HasMaxLength(100)
+            .IsRequired();
+        builder.Property(c => c.Birthdate)
+            .IsRequired();
+        builder.Property(c => c.Gender)
+            .HasMaxLength(30)
+            .IsRequired();
+        builder.Property(c => c.Species)
+            .IsRequired();
+        builder.Property(c => c.Height)
+            .HasPrecision(10,2)
+            .IsRequired();
+        builder.Property(c => c.HairColor)
+            .HasMaxLength(30)
+            .IsRequired();
+        builder.Property(c => c.EyeColor)
+            .HasMaxLength(30)
+            .IsRequired();
+        builder.Property(c => c.Description)
+            .HasMaxLength(2000)
+            .IsRequired();
         
         builder.HasMany(c => c.Movies)
             .WithMany(m => m.Characters)
--- src/star_wars.Infrastructure/Data/Configurations/MovieConfiguration.cs
+++ src/star_wars.Infrastructure/Data/Configurations/MovieConfiguration.cs
@@ -8,17 +8,17 @@ public class MovieConfiguration : IEntityTypeConfiguration<Movie>
 {
     public void Configure(EntityTypeBuilder<Movie> builder)
     {
-        // builder.HasKey(m => m.Id);
-        // builder.HasIndex(m => m.Id)
-        //     .IsUnique();
-        // builder.HasIndex(m => m.Title)
-        //     .IsUnique();
-        // builder.Property(m => m.Id)
-        //     .ValueGeneratedOnAdd();
+        builder.HasKey(m => m.Id);
+        builder.HasIndex(m => m.Id)
+            .IsUnique();
+        builder.Property(m => m.Id)
+            .ValueGeneratedOnAdd();
+        builder.HasIndex(m => m.Title)
+            .IsUnique();
 
-        // builder.Property(m => m.Title)
-        //     .HasMaxLength(100)
-        //     .IsRequired();
+        builder.Property(m => m.Title)
+            .HasMaxLength(100)
+            .IsRequired();
         
         builder.HasMany(c => c.Characters)
             .WithMany(m => m.Movies)
